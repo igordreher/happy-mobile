48735853f7b9e5eefbc218994895a3153d3c77ad
igordreher <igord.sparrenberger@gmail.com>
2020-10-17 18:51:37 -0300

    Added map page

--- App.tsx
+++ App.tsx
@@ -1,12 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
+import MapView, { PROVIDER_GOOGLE, Marker, Callout } from 'react-native-maps'
+import { Feather } from '@expo/vector-icons'
+
+import mapMarker from './src/images/map-marker.png'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Hello World</Text>
-      <StatusBar style="auto" />
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        initialRegion={{
+          latitude: -15.8401632,
+          longitude: -48.0476195,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        }}
+      >
+        <Marker
+          icon={mapMarker}
+          coordinate={{
+            latitude: -15.8401632,
+            longitude: -48.0476195,
+          }}
+        >
+          <Callout tooltip={true}>
+            <View style={styles.calloutContainer}>
+              <Text style={styles.calloutText}>Lar das meninas</Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
+
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>2 Orfanatos encontrados</Text>
+
+        <TouchableOpacity style={styles.createOrphanageButton}>
+          <Feather name='plus' size={20} color='#fff' />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -14,13 +47,50 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-  title: {
-    fontSize: 40,
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height
+  },
+  calloutContainer: {
+    width: 160,
+    height: 46,
+    paddingHorizontal: 16,
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    borderRadius: 16,
+    justifyContent: "center",
+  },
+  calloutText: {
     fontFamily: '',
-    fontWeight: 'bold'
+    color: '#0089a5',
+    fontSize: 14,
+  },
+  footer: {
+    position: 'absolute',
+    left: 24,
+    right: 24,
+    bottom: 32,
+    backgroundColor: '#fff',
+    borderRadius: 20,
+
+    height: 56,
+    paddingLeft: 24,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    elevation: 3,
+  },
+  footerText: {
+    fontFamily: '',
+    fontSize: 15,
+    color: '#8fa7b3'
+  },
+  createOrphanageButton: {
+    height: 56,
+    width: 56,
+    backgroundColor: '#15C3D6',
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 });
