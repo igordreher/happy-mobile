d3dea9d8714859e7daa3258dd470ebf4ae3a2e51
igordreher <igord.sparrenberger@gmail.com>
2020-10-18 10:33:09 -0300

    Create header component & added header to 3 screens

--- src/routes.tsx
+++ src/routes.tsx
@@ -7,12 +7,14 @@ import OrphanageDetails from './pages/OrphanageDetails'
 import SetPosition from './pages/CreateOrphanage/SetPosition'
 import OrphanageData from './pages/CreateOrphanage/OrphanageData'
 
+import Header from './components/Header'
+
 const { Navigator, Screen } = createStackNavigator()
 
 export default function Routes() {
     return (
         <NavigationContainer>
-            <Navigator screenOptions={{ headerShown: false }}>
+            <Navigator screenOptions={{ headerShown: false, cardStyle:{backgroundColor: '#f2f3f5'}}}>
                 <Screen
                     name='OrphanagesMap'
                     component={OrphanagesMap}
@@ -21,15 +23,27 @@ export default function Routes() {
                 <Screen
                     name='OrphanageDetails'
                     component={OrphanageDetails}
+                    options={{
+                        headerShown: true,
+                        header: () => <Header showCancel={false} title={'Orfanato'} />
+                    }}
                 />
 
                 <Screen
                     name='SetPosition'
                     component={SetPosition}
+                    options={{
+                        headerShown: true,
+                        header: () => <Header title={'Selecione a posição do orfanato'} />
+                    }}
                 />
                 <Screen
                     name='OrphanageData'
                     component={OrphanageData}
+                    options={{
+                        headerShown: true,
+                        header: () => <Header title={'Preencha o formulário'} />
+                    }}
                 />
             </Navigator>
         </NavigationContainer>
